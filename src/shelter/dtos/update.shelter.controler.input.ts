416cc1469b22e1a5fde196fe97f679f6678e34a7
Vitoriosb <vitorio.sb@homail.com>
2024-04-03 22:08:12 -0300

    Adicionado validação no input(shelter)

--- src/shelter/dtos/update.shelter.controler.input.ts
+++ src/shelter/dtos/update.shelter.controler.input.ts
@@ -1,6 +1,24 @@
+import {
+  IsEmail,
+  IsNotEmpty,
+  IsNumberString,
+  IsString,
+  Length,
+} from 'class-validator';
+
 export default class UpdateShelterControllerInput {
+  @IsString()
+  @IsNotEmpty()
   name: string;
+  @IsString()
+  @Length(11, 11)
+  @IsNotEmpty()
   whatsapp: string;
+  @Length(11, 11)
+  @IsNumberString()
+  @IsNotEmpty()
   phone: string;
+  @IsEmail()
+  @IsNotEmpty()
   email: string;
 }
