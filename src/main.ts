1dc4952ad21734dcf596774a7bce75b0bf34dcfd
Vitoriosb <vitorio.sb@homail.com>
2024-04-12 21:14:26 -0300

    adicionado swagger

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,22 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
+  app.enableCors();
+
+  const config = new DocumentBuilder()
+    .setTitle('Picles')
+    .setDescription('The picles API description')
+    .setVersion('1.0')
+    .addTag('picles')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
--- src/pet/dtos/create.pet.controller.input.ts
+++ src/pet/dtos/create.pet.controller.input.ts
@@ -1,24 +1,30 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsNotEmpty, IsString, MaxLength } from 'class-validator';
 
 export default class CreatePetControllerInput {
   @IsString()
   @IsNotEmpty()
+  @ApiProperty()
   name: string;
 
   @IsString()
   @IsNotEmpty()
+  @ApiProperty()
   type: string;
 
   @IsString()
   @IsNotEmpty()
+  @ApiProperty()
   size: string;
 
   @IsString()
   @IsNotEmpty()
+  @ApiProperty()
   gender: string;
 
   @IsString()
   @IsNotEmpty()
+  @ApiProperty()
   @MaxLength(1024)
   bio: string;
 }
